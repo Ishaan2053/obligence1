624218c51f8055f0b32b5dae5cd67d1de0e56327
Ishaan2053 <Ishaan2053@gmail.com>
2025-08-21 23:46:27 +0530

    feat: implement Layout, Page, and Dialog components with enhanced UI structure and styling

---
+++ apps/frontend/src/app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/dashboard/sidebar";
+import Header from "@/components/dashboard/header";
+
+export const metadata = {
+  title: "Dashboard - Obligence | AI-powered legal document extraction and review",
+  description: "Dashboard layout",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="pb-4">
+      <SidebarProvider>
+        <AppSidebar />
+
+        <main className="md:flex-1 space-y-2 overflow-hidden relative z-0">
+          <Header />
+          <div className="flex items-center justify-between border-b">
+            <SidebarTrigger className="flex md:hidden h-full" />
+          </div>
+          <div className="mx-4">{children}</div>
+        </main>
+      </SidebarProvider>
+    </main>
+  );
+}
