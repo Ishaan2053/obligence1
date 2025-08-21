ad683f21b4e4df812caea7e9af85fb816ba16989
Ishaan2053 <Ishaan2053@gmail.com>
2025-08-21 18:21:47 +0530

    feat: add login and registration pages with authentication forms

---
+++ apps/frontend/src/app/login/page.tsx
@@ -0,0 +1,36 @@
+import AuthForm from "@/components/auth/auth-form";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function LoginPage() {
+    return (
+        <>
+            <main className="flex min-h-screen">
+                <section className="flex w-full flex-col justify-center lg:w-1/2">
+                    <AuthForm variant="login" />
+
+                    <span className="text-neutral-400 text-center block mt-4">
+                        Don&apos;t have an account?{" "}
+                        <Link
+                            href="/register"
+                            className="text-white underline underline-offset-2"
+                        >
+                            Register
+                        </Link>
+                    </span>
+                </section>
+                <section className="hidden w-1/2 bg-neutral-900 lg:block">
+                    <div className="flex h-full items-center justify-center">
+                        <Image
+                            src="/images/login.jpg"
+                            width={800}
+                            height={800}
+                            alt="Login"
+                            className="h-screen object-cover grayscale brightness-50"
+                        />
+                    </div>
+                </section>
+            </main>
+        </>
+    );
+}
